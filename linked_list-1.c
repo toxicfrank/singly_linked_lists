daf0c54bc86d84d75cae4e02ace6f4b286d6d790
toxicfrank <ngugifrancis930@gmail.com>
2023-04-30 07:54:59 +0100

    practice

---
+++ linked_list-1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*typdef*/
+
+typedef struct node
+{
+    int a;
+    struct node * node;
+}node;
+
+/**
+ * main - return integers
+ * Retrun: always 0
+ */
+
+int main(int argc, char **argv)
+{
+
+    
+    return (0);
+}
+
---
+++ linked_list-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*typedef*/
+
+typedef struct node
+{
+    int a;
+    struct node *node;
+}node;
+
+/**
+ * main - return integer
+ * Return: always 0
+ */
+
+int main(int argc, char **argv)
+{
+
+    return (0);
+}
